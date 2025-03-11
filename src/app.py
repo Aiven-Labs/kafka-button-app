83f4f22be773a80dd462e3a8eebe002952ad5291
Tony Ibbs (Tibs) <tony.ibbs@aiven.io>
2025-03-11 14:07:47 +0000

    The app now sends avro messages to Kafka

--- src/app.py
+++ src/app.py
@@ -35,6 +35,7 @@ from pydantic import BaseModel
 from .button_responses import BUTTON_RESPONSES
 
 from .message_support import DEFAULT_TOPIC_NAME as TOPIC_NAME
+from .message_support import Action
 from .message_support import Event
 from .message_support import load_geoip_data
 from .message_support import create_avro_schema
@@ -125,7 +126,6 @@ app = FastAPI(
 templates = Jinja2Templates(directory="templates")
 
 session = uuid4()
-geoip = GeoIP2Fast()
 
 
 class ClickInteraction(BaseModel):
@@ -138,7 +138,7 @@ class ClickInteraction(BaseModel):
 
     def model_post_init(self, __context: Any) -> None:
         """Performs a lookup at the user's IP Address and returns the country of the user"""
-        lookup = geoip.lookup(self.ip_address)
+        lookup = lifespan_data.geoip.lookup(self.ip_address)
 
         self.country = lookup.country_name
         self.longitude = 0.0
@@ -156,15 +156,19 @@ def get_client_ip(request: Request) -> str:
         return request.client.host if request.client else "unknown"
 
 
+# These will get moved to the top (or deleted if not needed) when things are working
 import json
 import uuid
 from fastapi.responses import JSONResponse
+from geoip2fast.geoip2fast import GeoIPError
 from pydantic import ValidationError
 
 COOKIE_NAME = 'button_press_session'
 COOKIE_LIFETIME = 3600    # 1 hour
 
 DEFAULT_COHORT = 0
+
+FAKE_DATA = True
 
 
 class Cookie(BaseModel):
@@ -177,19 +181,50 @@ class Cookie(BaseModel):
     city_name: str           # may be ''
 
 
-def new_cookie():
+def new_cookie(request: Request) -> Cookie:
+    """Calculate the cookie for a new 'session'
+    """
+    ip_address = get_client_ip(request)
+    if FAKE_DATA and ip_address == '127.0.0.1':
+        # Because localhost isn't much fun...
+        ip_address = lifespan_data.geoip.generate_random_ipv4_address()
+    try:
+        geoip_data = lifespan_data.geoip.lookup(ip_address)
+    except GeoIPError as e:
+        logging.error(f'IP lookup error: {e}')
+        logging.error(f'Trying to lookup {ip_address}')
+        raise ValueError('Unable to retrieve IP data {e} for {ip_address}')
+
     return Cookie(
         session_id=str(uuid.uuid4()),
         cohort=DEFAULT_COHORT,
-        country_name='France',
-        country_code='FR',
-        subdivision_name='',
-        subdivision_code='',
-        city_name='',
+        country_name=geoip_data.country_name,
+        country_code=geoip_data.country_code,
+        subdivision_name=geoip_data.city.subdivision_name,
+        subdivision_code=geoip_data.city.subdivision_code,
+        city_name=geoip_data.city.name,
     )
 
 
-@app.get("/reset", response_class=JSONResponse)
+async def send_avro_message(cookie: Cookie, action: Action):
+    """Send an Avro encoded message based on the cookie."""
+
+    event = Event(
+        **dict(cookie),
+        timestamp=datetime.datetime.now(datetime.timezone.utc).isoformat(),
+        action=action,
+    )
+    logging.info(f'Sending Avro message {event}')
+    message_bytes = make_avro_payload(
+        event,
+        lifespan_data.avro_schema_id,
+        lifespan_data.parsed_avro_schema,
+    )
+    # For the moment, don't let it buffer messages
+    await lifespan_data.producer.send_and_wait(TOPIC_NAME, message_bytes)
+
+
+@app.get("/reset", response_class=HTMLResponse)
 async def reset(request: Request):
     """For development/debugging - reset any cookies when we're visited"""
 
@@ -199,9 +234,32 @@ async def reset(request: Request):
 
     logging.info('Expiring cookie values')
 
-    response = JSONResponse(content= {'message': 'Cookies unset'})
+    response = HTMLResponse("""\
+    <html>
+    <head><h1>Cookies are reset</h1></head>
+    <body><p><a href="/">Back to the button</a></p>
+    </html>
+    """)
     response.delete_cookie(key=COOKIE_NAME)
     return response
+
+
+def get_cookie_from_request(request: Request) -> Cookie:
+    """Get the cookie from the request. If there isn't one, make a new one.
+    """
+    logging.info(f'Retrieving cookie {COOKIE_NAME}')
+    cookie_str = request.cookies.get(COOKIE_NAME)
+    if not cookie_str:
+        logging.info(f'No cookie found: making new')
+        cookie = new_cookie(request)
+    else:
+        logging.info(f'Found cookie value {cookie_str}')
+        try:
+            cookie = Cookie.model_validate_json(cookie_str)
+        except ValidationError:
+            logging.error(f'Unable to parse cookie {COOKIE_NAME} value {cookie_str}')
+            cookie = new_cookie(request)
+    return cookie
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -211,24 +269,22 @@ async def get_index(request: Request):
     logging.info('HI HI INDEX')
     logging.info(f'request.cookies {request.cookies}')
 
+    cookie = get_cookie_from_request(request)
+
+    # Send our actual message to Kafka
+    await send_avro_message(cookie, Action.PRESS_BUTTON)
+
     context = {
         "request": request,
         "button_response": "Clicking this button does absolutely nothing.",
     }
-
     response = templates.TemplateResponse("index.html", context)
 
-    cookie_str = request.cookies.get(COOKIE_NAME)
-    if cookie_str:
-        logging.info(f'Already got cookie {request.cookies.get(COOKIE_NAME)}')
-        # Each time the page is visited/refreshed, extend the cookie lifetime
-        logging.info(f'Extending cookie lifetime')
-        response.set_cookie(key=COOKIE_NAME, value=cookie_str, expires=COOKIE_LIFETIME)
-    else:
-        cookie = new_cookie()
-        cookie_str = cookie.model_dump_json()
-        logging.info(f'Setting new cookie {cookie_str}')
-        response.set_cookie(key=COOKIE_NAME, value=cookie_str, expires=COOKIE_LIFETIME)
+    # We always (re)set the cookie
+    # If there wasn't one before, we need to set the new one now
+    # If it already existed, we want to extend its lifetime
+    cookie_str = cookie.model_dump_json()
+    response.set_cookie(key=COOKIE_NAME, value=cookie_str, expires=COOKIE_LIFETIME)
 
     return response
 
@@ -242,36 +298,26 @@ async def send_ip(request: Request):
     logging.info('HI HI BUTTON')
     logging.info(f'request.cookies {request.cookies}')
 
-    cookie_str = request.cookies.get(COOKIE_NAME)
-    if cookie_str:
-        logging.info(f'Unpacking cookie {COOKIE_NAME} value {cookie_str!r}')
-        try:
-            cookie = Cookie.model_validate_json(cookie_str)
-        except ValidationError:
-            logging.error(f'Unable to parse cookie {COOKIE_NAME} value {cookie_str}')
-            cookie = new_cookie()
-    else:
-        # Can this happen? Let's cope if it does
-        cookie = new_cookie()
+    cookie = get_cookie_from_request(request)
 
     ip_address = get_client_ip(request)
     interaction = ClickInteraction(
         ip_address=ip_address, timestamp=datetime.datetime.now(datetime.timezone.utc)
     )
 
-    # Send to Kafka
-    # Create message with IP and timestamp
-    message = interaction.model_dump_json(exclude=ip_address)
-    logging.info(message)
-    # Send to Kafka
+    # Send our actual message to Kafka
+    await send_avro_message(cookie, Action.ENTER_PAGE)
 
     # Return only the updated part for HTMX to replace
-
-    context = {"request": request, "button_response": random.choice(BUTTON_RESPONSES)}
+    context = {
+        "request": request,
+        "button_response": random.choice(BUTTON_RESPONSES),
+    }
     response = templates.TemplateResponse("partials/button_text.html", context)
 
-    # Each time the button is pressed, extend the cookie lifetime
-    logging.info(f'Extending cookie lifetime')
+    # We always (re)set the cookie
+    # If there wasn't one before, we need to set the new one now
+    # If it already existed, we want to extend its lifetime
     cookie_str = cookie.model_dump_json()
     response.set_cookie(key=COOKIE_NAME, value=cookie_str, expires=COOKIE_LIFETIME)
 
