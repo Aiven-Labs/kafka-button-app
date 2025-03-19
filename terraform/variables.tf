4b30d3e360ddc08def3fe3fc08aef6ea09e5ab9a
Jay Miller <kjaymiller@gmail.com>
2025-03-19 17:19:13 -0400

    remove hardcoded choices for project and region

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,7 +9,7 @@ variable "aiven_project_name" {
 }
 
 variable "cloud_name" {
-  description = "The cloud provider and region for Aiven services"
+  description = "The cloud provider and region for Aiven services. Example: google-us-east-1"
   type        = string
 }
 
