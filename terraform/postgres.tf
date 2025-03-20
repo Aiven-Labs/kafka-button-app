31e38d5d600d18961e8c85f26c97a86253238903
Jay Miller <kjaymiller@gmail.com>
2025-03-20 13:52:49 -0400

    adds create kafka topic

--- terraform/postgres.tf
+++ terraform/postgres.tf
@@ -1,13 +1,7 @@
 resource "aiven_pg" "pg_button_app" {
   project      = data.aiven_project.button_app.project
   cloud_name   = var.cloud_name
-  plan         = "startup-4"
+  plan         = "hobbyist"
   service_name = "${var.pg_service_name}-${random_string.suffix.result}"
 
 }
-
-output "pg_service_endpoint_uri" {
-  value     = aiven_pg.pg_button_app.service_uri
-  sensitive = true
-}
-
