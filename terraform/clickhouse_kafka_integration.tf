fc13e023e1ed07140c2bee9c4200ed03f64d9212
Jay Miller <kjaymiller@gmail.com>
2025-03-25 12:20:37 -0400

    update clickhouse integration to add table info

--- terraform/clickhouse_kafka_integration.tf
+++ terraform/clickhouse_kafka_integration.tf
@@ -1,6 +1,55 @@
+
 resource "aiven_service_integration" "clickhouse_kafka_integration" {
   project                  = data.aiven_project.button_app.project
   destination_service_name = aiven_clickhouse.ch_button_app.service_name
   integration_type         = "clickhouse_kafka"
   source_service_name      = aiven_kafka.kafka_button_app.service_name
+  clickhouse_kafka_user_config {
+    tables {
+      name        = "button_presses_from_kafka"
+      data_format = "AvroConfluent"
+      group_name  = "button_presses_from_kafka_consumer"
+
+      topics {
+        name = "button_presses"
+      }
+
+      columns {
+        name = "session_id"
+        type = "UUID"
+      }
+      columns {
+        name = "timestamp"
+        type = "DateTime64(6)"
+      }
+      columns {
+        name = "action"
+        type = "String"
+      }
+      columns {
+        name = "country_name"
+        type = "String"
+      }
+      columns {
+        name = "country_code"
+        type = "FixedString(2)"
+      }
+      columns {
+        name = "subdivision_name"
+        type = "String"
+      }
+      columns {
+        name = "subdivision_code"
+        type = "String"
+      }
+      columns {
+        name = "city_name"
+        type = "String"
+      }
+      columns {
+        name = "cohort"
+        type = "Nullable(Smallint)"
+      }
+    }
+  }
 }
