240544fd2d30763765670c880c90bfca0ef86234
Jay Miller <kjaymiller@gmail.com>
2025-05-01 09:23:09 -0400

    revert docker commands

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,7 @@ WORKDIR /app
 COPY ./requirements.txt /app
 COPY ./run.sh /app
 
-RUN apt-get update &&\
-  apt-get install -y curl jq
+RUN apt-get update && apt-get install -y curl jq
 
 RUN python3 -m pip install --no-cache-dir -r requirements.txt
 
@@ -16,11 +15,6 @@ COPY ./src /app/src
 
 # Create somewhere for the Kafka certificates
 RUN mkdir /app/certs
-
-# Download the geoip2fast dataset that knows about cities
-# Note the `-L` (--location) to follow redirects, without which it won't work
-## DISABLED FOR THE MOMENT
-##RUN curl https://github.com/rabuchaim/geoip2fast/releases/download/LATEST/geoip2fast-city-ipv6.dat.gz -L --output /app/geoip2fast-city-ipv6.dat.gz
 
 EXPOSE 3000
 CMD [ "bash", "./run.sh" ]
--- run.sh
+++ run.sh
@@ -1,18 +1,50 @@
 #!/usr/bin/env sh
 
-# Create PG and CH tables
-./setup_scripts/create_pg_table.sh
-./setup_scripts/create_clickhouse_tables.sh
+# Set up the environment for our app, and then run it
 
-# Download Kafka certs from avn client
-# AIVEN_TOKEN env variable needs to be assigned
-# TODO: Check if there is a default ENVIRONMENT VARIABLE THAT WORKS WITH AVN CLI
+## The example from https://github.com/Aiven-Labs/fake-data-producer-for-apache-kafka-docker
+## reads the environment variables from a file. Otherwise, we might pass them in to the
+## container *as* environment variables
+##
+## source /app/env.conf
+##
+## We expect the following environment variables to already be set
+##
+## AIVEN_TOKEN
+## PROJECT_NAME
+## KAFKA_SERVICE_NAME
+## CH_SERVICE_NAME
+
+# Kafka
+
+export KAFKA_SERVICE_URI=$(avn --auth-token "$AIVEN_TOKEN" service get \
+  --project $PROJECT_NAME \
+  $KAFKA_SERVICE_NAME --format '{service_uri}')
+
 # Kafka certificates
-# Existing certs will be overwritten
-
+rm -rf ./certs
 avn --auth-token "$AIVEN_TOKEN" service user-creds-download \
   --project $PROJECT_NAME \
   $KAFKA_SERVICE_NAME --username avnadmin -d ./certs
 
-# run the application
+# Schema registry
+export SCHEMA_REGISTRY_URI=$(avn --auth-token "$AIVEN_TOKEN" service get \
+  --project $PROJECT_NAME \
+  $KAFKA_SERVICE_NAME --json | jq -r '.connection_info.schema_registry_uri')
+
+# ClickHouse
+export CH_HOST=$(avn --auth-token "$AIVEN_TOKEN" service get \
+  --project $PROJECT_NAME \
+  $CH_SERVICE_NAME --json | jq -r '.components | map(select(.component == "clickhouse_https"))[0].host')
+export CH_PORT=$(avn --auth-token "$AIVEN_TOKEN" service get \
+  --project $PROJECT_NAME \
+  $CH_SERVICE_NAME --json | jq -r '.components | map(select(.component == "clickhouse_https"))[0].port')
+export CH_USERNAME=$(avn --auth-token "$AIVEN_TOKEN" service get \
+  --project $PROJECT_NAME \
+  $CH_SERVICE_NAME --json | jq -r '.service_uri_params.user')
+export CH_PASSWORD=$(avn --auth-token "$AIVEN_TOKEN" service get \
+  --project $PROJECT_NAME \
+  $CH_SERVICE_NAME --json | jq -r '.service_uri_params.password')
+
+# And finally run the application
 fastapi run src/app.py --port 3000
